4221b2936f72130b3f88b341f3cff30f1c1a7023
markoMagdy1 <markomagdy1231@gmail.com>
2023-02-21 03:01:46 +0200

    add page car details

---
+++ src/components/car_details/carDetails.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import Figure from "react-bootstrap/Figure";
+import ".././css/details.css";
+
+export default function CarDetails() {
+  const imgs = [
+    { id: 0, value: "https://wallpaperaccess.com/full/2637581.jpg" },
+    { id: 1, value: "https://source.unsplash.com/user/c_v_r/1900x800" },
+    {
+      id: 2,
+      value:
+        "https://c4.wallpaperflare.com/wallpaper/910/24/10/night-artwork-futuristic-city-cyberpunk-wallpaper-thumb.jpg",
+    },
+    {
+      id: 3,
+      value:
+        "https://c4.wallpaperflare.com/wallpaper/910/24/10/night-artwork-futuristic-city-cyberpunk-wallpaper-thumb.jpg",
+    },
+    {
+      id: 4,
+      value:
+        "https://c4.wallpaperflare.com/wallpaper/663/947/813/oldboy-japanese-digital-art-artwork-wallpaper-thumb.jpg",
+    },
+    {
+      id: 5,
+      value:
+        "https://c4.wallpaperflare.com/wallpaper/910/24/10/night-artwork-futuristic-city-cyberpunk-wallpaper-thumb.jpg",
+    },
+  ];
+  const [wordData, setWordData] = useState(imgs[0]);
+  const handleClick = (index) => {
+    console.log(index);
+    const wordSlider = imgs[index];
+    setWordData(wordSlider);
+  };
+  return (
+    <div className="main">
+      <img src={wordData.value} className="p" />
+      <div className="flex_row">
+        {imgs.map((data, i) => (
+          <div className="thumbnail" key={i}>
+            <img
+              className={wordData.id == i ? "clicked" : ""}
+              id="po"
+              src={data.value}
+              onClick={() => handleClick(i)}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
