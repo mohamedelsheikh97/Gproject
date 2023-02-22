422f44f32b5910c782a35618c3f29c23a9546488
OmarElshankery <omarelshankery@gmail.com>
2023-02-22 18:47:30 +0200

    new

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Footer from "./components/footer";
 import Maintenance from "./components/maintain_page/maintain";
 import School from "./components/schools_page/school";
 import Notfound from "./components/notfound_page/notfound";
+import Login from "./components/login_page/login";
+import Accessories from "./components/accessories_page/accessories";
 
 function App() {
   return (
@@ -19,6 +21,8 @@ function App() {
         <Route path="/cardetails" element={<CarDetails />} />
         <Route path="/maintenance" element={<Maintenance />} />
         <Route path="/schools" element={<School />} />
+        <Route path="/accessories" element={<Accessories />} />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
       <Footer></Footer>
---
+++ src/components/accessories_page/accessories.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Accessories() {
+  return <div className="text-center">accessories page</div>;
+}
