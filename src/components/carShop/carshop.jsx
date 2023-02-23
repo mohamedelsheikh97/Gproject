3f289e683f4054e59864989ee322e205eefbc30a
sherifhesham74 <sherifhesham74@gmail.com>
2023-02-23 17:59:59 +0200

    تعديل علي الكار شوب تم اضافة كاردات رايقة ذات انيميشن فاخر من الاخر

--- src/components/carShop/carshop.jsx
+++ src/components/carShop/carshop.jsx
@@ -1,87 +1,245 @@
-import React from 'react'
-import '../../css/car_shop/carshop.css'
+import React from "react";
+import "../../css/car_shop/carshop.css";
 
 export default function Carshop() {
   return (
-    <div className='kk'>
-        <section>
+    <div className="kk">
+      <section>
+        <div class="container-fluid intro">
+          <img src="/kersh.png" id="c-wrench" />
+          <ul>
+            <li className="big">
+              <a
+                href="https://www.facebook.com/elkershcars.eg/"
+                target={"_blank"}
+              >
+                <i class="fab fa-facebook-square fa-2x" aria-hidden="true"></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/elkershcars.official/"
+                target={"_blank"}
+              >
+                <i class="fab fa-instagram-square fa-2x" aria-hidden="true"></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.google.com/maps/place/El+Kersh+Cars+Official/data=!4m7!3m6!1s0x14583f39289fec69:0x94b8b87ed5aec655!8m2!3d30.083923!4d31.3259021!16s%2Fg%2F1tcttvfd!19sChIJaeyfKDk_WBQRVcau1X64uJQ?authuser=0&hl=ar&rclk=1"
+                target={"_blank"}
+              >
+                <i class="fas fa-thumbtack fa-2x" aria-hidden="true"></i>
+              </a>
+            </li>
+          </ul>
+          <section class="ourWork" id="ourWork">
+            {/* <h2>OUR Cars</h2> */}
+            {/* <div class="loader">
+  Our Cars
   
-  <div class="container-fluid intro" >
-   <img src="/kersh.png" id='c-wrench' />
-   <ul>
-    <li className='big'><a href="https://www.facebook.com/elkershcars.eg/" target={'_blank'}><i class="fab fa-facebook-square fa-2x" aria-hidden="true"></i></a></li>
-    <li><a href="https://www.instagram.com/elkershcars.official/" target={'_blank'}><i class="fab fa-instagram-square fa-2x" aria-hidden="true" ></i></a></li>
-    <li><a href="https://www.google.com/maps/place/El+Kersh+Cars+Official/data=!4m7!3m6!1s0x14583f39289fec69:0x94b8b87ed5aec655!8m2!3d30.083923!4d31.3259021!16s%2Fg%2F1tcttvfd!19sChIJaeyfKDk_WBQRVcau1X64uJQ?authuser=0&hl=ar&rclk=1" target={'_blank'}><i class="fas fa-thumbtack fa-2x" aria-hidden="true" ></i></a></li>
-   </ul>
-   <section class="ourWork" id="ourWork">
-    <h2>OUR Cars</h2>
-    <hr class="black"/>
+</div> */}
+<div class="hh">
+  <div class="typewriter">Our Cars .....</div>
+</div>
+            <hr class="black" />
 
-    <div class="container-fluid">
-       <div class="row">
-         <div class="col-xs-4 workimg">   
-          <img class="img-responsive" src="https://scontent.fcai19-6.fna.fbcdn.net/v/t39.30808-6/326061568_483099513795698_7630845182300620984_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_ohc=UOA7lDDG_8UAX9ZF_kv&_nc_ht=scontent.fcai19-6.fna&oh=00_AfCilhohpRImGMlcEJ1IhJ3mWw4BhFWmLlSE-FsS_1x3Cw&oe=63FB692A"/>
-            <div><button class="button accept-btn"  onclick="addAnimation2()"> 
-<i class="fas fa-car"></i> <npsp></npsp>
-Car Details
-</button>
-</div>
-       </div>
-       <div class="col-xs-4 workimg">
-          <img class="img-responsive" src="https://scontent.fcai19-6.fna.fbcdn.net/v/t39.30808-6/325410338_474901354629282_1322276654709657236_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=EFBg0PhA-wkAX8a8sy1&_nc_ht=scontent.fcai19-6.fna&oh=00_AfAmYHkiPnBQuqINKEFcc9m5_p9iJnuc-yMh7J8Uw-GkuQ&oe=63FBACAD"/>
-          <div><button class="button accept-btn"  onclick="addAnimation2()"> 
-<i class="fas fa-car"></i> <npsp></npsp>
-Car Details
-</button>
-</div>
-       </div>
-       <div class="col-xs-4 workimg">
-          <img class="img-responsive" src="https://scontent.fcai19-6.fna.fbcdn.net/v/t39.30808-6/322251329_582180193744707_2732749201331829696_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_ohc=lF2vy7_1l7UAX_nsQRq&_nc_ht=scontent.fcai19-6.fna&oh=00_AfDSRji2f9nP2WX8HIJ-XlU4sN0enuQrmEuBCsvgzEeBNw&oe=63FC8185"/>
-          <div><button class="button accept-btn"  onclick="addAnimation2()"> 
-<i class="fas fa-car"></i> <npsp></npsp>
-Car Details
-</button>
-</div>
-       </div>
-     </div>
-     </div>
-     <div class="container-fluid">
-       <div class="row">
-         <div class="col-xs-4 workimg">
-        
-          <img class="img-responsive" src="https://scontent.fcai19-6.fna.fbcdn.net/v/t39.30808-6/321558918_618598943367101_7512442809510969924_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=x2JQYjT0EfsAX-1IfOc&_nc_ht=scontent.fcai19-6.fna&oh=00_AfB2pmFyqdoHqSzhHE2evEt5dC-fG0cj5ZGOfDTiI1aNcw&oe=63FC2BE7"/>
-          <div><button class="button accept-btn"  onclick="addAnimation2()"> 
-<i class="fas fa-car"></i> <npsp></npsp>
-Car Details
-</button>
-</div>
-       </div>
-       <div class="col-xs-4 workimg">
-          <img  class="img-responsive" src="https://scontent.fcai19-6.fna.fbcdn.net/v/t39.30808-6/321654822_581279163906628_6404364169687849443_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=gfEQTWYiBLIAX_ywmwp&tn=jYyjmWf80nXja3EL&_nc_ht=scontent.fcai19-6.fna&oh=00_AfCphsb6uqGqpXM6DYg-OK37CV3CZCsSxjIztNCp6Ku_6A&oe=63FC3AEA"/>
-          <div><button class="button accept-btn"  onclick="addAnimation2()"> 
-<i class="fas fa-car"></i> <npsp></npsp>
-Car Details
-</button>
-</div>
-       </div>
-       <div class="col-xs-4 workimg">
-        
-          <img class="img-responsive" src="https://scontent.fcai19-6.fna.fbcdn.net/v/t39.30808-6/321424992_807532877012554_3481564339408091151_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_ohc=ME1YyJ8zGkIAX_n8qli&_nc_ht=scontent.fcai19-6.fna&oh=00_AfCh1g23aEePoD8vYlTkQ3ytL785o35T8JqZy9VoVPXzRQ&oe=63FB4F06"/>
-          <div><button class="button accept-btn"  onclick="addAnimation2()"> 
-<i class="fas fa-car"></i> <npsp></npsp>
-Car Details
-</button>
-</div>
-       </div>
-    </div>
-    </div>
-  </section>
-
-  </div>
-
-</section>
- 
+            <div class="container ">
+              <div class="row">
+                <div className="col-4">
+              <div class="wrapper ">
+      <div class="card front-face">
+        <img src="https://scontent.fcai19-6.fna.fbcdn.net/v/t39.30808-6/321424992_807532877012554_3481564339408091151_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_ohc=ME1YyJ8zGkIAX_n8qli&_nc_ht=scontent.fcai19-6.fna&oh=00_AfCh1g23aEePoD8vYlTkQ3ytL785o35T8JqZy9VoVPXzRQ&oe=63FB4F06" alt="Flip Card"/>
+      </div>
+      <div class="card back-face">
+        <img src="https://scontent.fcai19-6.fna.fbcdn.net/v/t39.30808-6/321424992_807532877012554_3481564339408091151_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_ohc=ME1YyJ8zGkIAX_n8qli&_nc_ht=scontent.fcai19-6.fna&oh=00_AfCh1g23aEePoD8vYlTkQ3ytL785o35T8JqZy9VoVPXzRQ&oe=63FB4F06" alt="Flip Card"/>
+        <div class="info">
+        <div class="title">BMW X8</div>
+          <h4> 2023    </h4> <br/>
+<br />
         </div>
+  
+        <button class="button accept-btn">
+More Details
+</button>
+<br/> <br />
+        <ul>
+          <a href="#"><i class="fab fa-facebook-f"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-youtube"></i></a>
+        </ul>
+      </div>
+    </div>
+    </div>
+    <div className="col-4">
+    <div class="wrapper ">
+      <div class="card front-face">
+        <img src="https://scontent.fcai19-6.fna.fbcdn.net/v/t39.30808-6/326793027_729863231898899_3970952357772941382_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_ohc=RoSaAjeM-C0AX8Gv6vb&_nc_ht=scontent.fcai19-6.fna&oh=00_AfD4DQYj1zqhyuwZFDLPM7fUajRr6S3WlisVPN5j_TdUBw&oe=63FB8BE9" alt="Flip Card"/>
+      </div>
+      <div class="card back-face">
+        <img src="https://scontent.fcai19-6.fna.fbcdn.net/v/t39.30808-6/326793027_729863231898899_3970952357772941382_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_ohc=RoSaAjeM-C0AX8Gv6vb&_nc_ht=scontent.fcai19-6.fna&oh=00_AfD4DQYj1zqhyuwZFDLPM7fUajRr6S3WlisVPN5j_TdUBw&oe=63FB8BE9" alt="Flip Card"/>
+        <div class="info">
+          <div class="title">BMW X8</div>
+          <h4> 2023    </h4> <br/>
+<br />
+        </div>
+  
+        <button class="button accept-btn">
+More Details
+</button>
+<br/> <br />
+        <ul>
+          <a href="#"><i class="fab fa-facebook-f"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-youtube"></i></a>
+        </ul>
+      </div>
+    </div>
+    </div>
+    <div className="col-4">
+    <div class="wrapper ">
+      <div class="card front-face">
+        <img src="https://scontent.fcai19-6.fna.fbcdn.net/v/t39.30808-6/326032120_758501412366701_4769919662020092655_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_ohc=X-I7agTiBu8AX9RQm2n&_nc_ht=scontent.fcai19-6.fna&oh=00_AfA7ehKIxEMdqpJudOAwc6qK8dTP_fFI1h4ZXF1gQuYBag&oe=63FCAB6E" alt="Flip Card"/>
+      </div>
+      <div class="card back-face">
+        <img src="https://scontent.fcai19-6.fna.fbcdn.net/v/t39.30808-6/329217842_713880490461081_8206761551032935767_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=NaaBc95EWz4AX93NeID&_nc_ht=scontent.fcai19-6.fna&oh=00_AfAWUyGg6XI-8p4HyZjo5dlfemYS2fbOxuDSITuFrZRLvg&oe=63FC5646" alt="Flip Card"/>
+        <div class="info">
+        <div class="title">BMW X8</div>
+          <h4> 2023    </h4> <br/>
+<br />
+        </div>
+  
+        <button class="button accept-btn">
+More Details
+</button>
+<br/> <br />
+        <ul>
+          <a href="#"><i class="fab fa-facebook-f"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-youtube"></i></a>
+        </ul>
+      </div>
+    </div>
+    </div>
+    <div className="col-4">
+    <div class="wrapper ">
+      <div class="card front-face">
+        <img src="https://scontent.fcai19-6.fna.fbcdn.net/v/t39.30808-6/323139485_659244095911978_6512380118674075603_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=ave2DzqzuI0AX9Z52df&_nc_ht=scontent.fcai19-6.fna&oh=00_AfBc7yuj7s7FtudckM8cMljNii9fp9b9mL4kio9v8YJxkQ&oe=63FBE976" alt="Flip Card"/>
+      </div>
+      <div class="card back-face">
+        <img src="https://scontent.fcai19-6.fna.fbcdn.net/v/t39.30808-6/323139485_659244095911978_6512380118674075603_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=ave2DzqzuI0AX9Z52df&_nc_ht=scontent.fcai19-6.fna&oh=00_AfBc7yuj7s7FtudckM8cMljNii9fp9b9mL4kio9v8YJxkQ&oe=63FBE976" alt="Flip Card"/>
+        <div class="info">
+        <div class="title">BMW X8</div>
+          <h4> 2023    </h4> <br/>
+<br />
+        </div>
+  
+        <button class="button accept-btn">
+More Details
+</button>
+<br/> <br />
+        <ul>
+          <a href="#"><i class="fab fa-facebook-f"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-youtube"></i></a>
+        </ul>
+      </div>
+    </div>
+    </div>
+    <div className="col-4">
+    <div class="wrapper ">
+      <div class="card front-face">
+        <img src="https://scontent.fcai19-6.fna.fbcdn.net/v/t39.30808-6/325776821_728243305623715_484930446275684348_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_ohc=hrkuPN-qTgcAX8jEb3K&_nc_ht=scontent.fcai19-6.fna&oh=00_AfAFFLctXdkJAtnY98RCHpYxcEs33Go_jBRRGONHN8Iu0g&oe=63FB945F" alt="Flip Card"/>
+      </div>
+      <div class="card back-face">
+        <img src="https://scontent.fcai19-6.fna.fbcdn.net/v/t39.30808-6/325776821_728243305623715_484930446275684348_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_ohc=hrkuPN-qTgcAX8jEb3K&_nc_ht=scontent.fcai19-6.fna&oh=00_AfAFFLctXdkJAtnY98RCHpYxcEs33Go_jBRRGONHN8Iu0g&oe=63FB945F" alt="Flip Card"/>
+        <div class="info">
+        <div class="title">BMW X8</div>
+          <h4> 2023    </h4> <br/>
+<br />
+        </div>
+  
+        <button class="button accept-btn">
+More Details
+</button>
+<br/> <br />
+        <ul>
+          <a href="#"><i class="fab fa-facebook-f"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-youtube"></i></a>
+        </ul>
+      </div>
+    </div>
+    </div>
+    <div className="col-4">
+    <div class="wrapper ">
+      <div class="card front-face">
+        <img src="https://scontent.fcai19-6.fna.fbcdn.net/v/t39.30808-6/326138003_572228168102195_1594439134533116712_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=KwLcZp9vr50AX_zJHc_&_nc_ht=scontent.fcai19-6.fna&oh=00_AfD6nA0QKCuqiT9SoOaWZADCF3uq3YyU5H2YeRhkvblSlQ&oe=63FBD66E" alt="Flip Card"/>
+      </div>
+      <div class="card back-face">
+        <img src="https://scontent.fcai19-6.fna.fbcdn.net/v/t39.30808-6/326138003_572228168102195_1594439134533116712_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=KwLcZp9vr50AX_zJHc_&_nc_ht=scontent.fcai19-6.fna&oh=00_AfD6nA0QKCuqiT9SoOaWZADCF3uq3YyU5H2YeRhkvblSlQ&oe=63FBD66E" alt="Flip Card"/>
+        <div class="info">
+        <div class="title">BMW X8</div>
+          <h4> 2023    </h4> <br/>
+<br />
+        </div>
+  
+        <button class="button accept-btn">
+More Details
+</button>
+<br/> <br />
+        <ul>
+          <a href="#"><i class="fab fa-facebook-f"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-youtube"></i></a>
+        </ul>
+      </div>
+    </div>
+    </div>
 
-  )
+{/* 
+    <div class="wrapper col-4">
+      <div class="card front-face">
+        <img src="https://scontent.fcai19-6.fna.fbcdn.net/v/t39.30808-6/321424992_807532877012554_3481564339408091151_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_ohc=ME1YyJ8zGkIAX_n8qli&_nc_ht=scontent.fcai19-6.fna&oh=00_AfCh1g23aEePoD8vYlTkQ3ytL785o35T8JqZy9VoVPXzRQ&oe=63FB4F06" alt="Flip Card"/>
+      </div>
+      <div class="card back-face">
+        <img src="https://scontent.fcai19-6.fna.fbcdn.net/v/t39.30808-6/321424992_807532877012554_3481564339408091151_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_ohc=ME1YyJ8zGkIAX_n8qli&_nc_ht=scontent.fcai19-6.fna&oh=00_AfCh1g23aEePoD8vYlTkQ3ytL785o35T8JqZy9VoVPXzRQ&oe=63FB4F06" alt="Flip Card"/>
+        <div class="info">
+          <div class="title">BMW</div>
+          <p>2023  <br/>X8</p>
+        </div>
+        <ul>
+          <a href="#"><i class="fab fa-facebook-f"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-youtube"></i></a>
+        </ul>
+      </div>
+    </div>
+    <div class="wrapper col-4">
+      <div class="card front-face">
+        <img src="https://scontent.fcai19-6.fna.fbcdn.net/v/t39.30808-6/321424992_807532877012554_3481564339408091151_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_ohc=ME1YyJ8zGkIAX_n8qli&_nc_ht=scontent.fcai19-6.fna&oh=00_AfCh1g23aEePoD8vYlTkQ3ytL785o35T8JqZy9VoVPXzRQ&oe=63FB4F06" alt="Flip Card"/>
+      </div>
+      <div class="card back-face">
+        <img src="https://scontent.fcai19-6.fna.fbcdn.net/v/t39.30808-6/321424992_807532877012554_3481564339408091151_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_ohc=ME1YyJ8zGkIAX_n8qli&_nc_ht=scontent.fcai19-6.fna&oh=00_AfCh1g23aEePoD8vYlTkQ3ytL785o35T8JqZy9VoVPXzRQ&oe=63FB4F06" alt="Flip Card"/>
+        <div class="info">
+          <div class="title">BMW</div>
+          <p>2023  <br/>X8</p>
+        </div>
+        <ul>
+          <a href="#"><i class="fab fa-facebook-f"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-youtube"></i></a>
+        </ul>
+      </div>
+    </div> */}
+              </div>
+            </div>
+          </section>
+        </div>
+      </section>
+   
+    </div>
+  );
 }
