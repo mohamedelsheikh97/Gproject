6006808f9a2d6f526339f9a0a60c7cb48949d3b4
mohamedelsheikh97 <mohamedali9756@gmail.com>
2023-02-24 02:48:06 +0200

    added shop and services section to home page

--- src/components/home_page/Header.jsx
+++ src/components/home_page/Header.jsx
@@ -4,14 +4,34 @@ import CarSlider from "./carSlider";
 import { Brands } from "./brands";
 import Welcome from "./welcome";
 import { Cards } from "./cards";
+import ReactCardSlider from "./reactCardSlider";
+import Servives from "./services";
 
 const Header = () => {
+  const sliderClick = (slider)=>{
+    alert("hello world");
+  }
+
+  
+  const slides = [
+    {image:"/اكسسوارات.png",title:"Elkersh motors",clickEvent:sliderClick},
+    {image:"https://picsum.photos/600/500",title:"This is a second title",description:"This is a second description",clickEvent:sliderClick},
+    {image:"https://picsum.photos/700/600",title:"This is a third title",description:"This is a third description",clickEvent:sliderClick},
+    {image:"https://picsum.photos/500/400",title:"This is a fourth title",description:"This is a fourth description",clickEvent:sliderClick},
+    {image:"https://picsum.photos/200/300",title:"This is a fifth title",description:"This is a fifth description",clickEvent:sliderClick},
+    {image:"https://picsum.photos/800/700",title:"This is a sixth title",description:"This is a sixth description",clickEvent:sliderClick},
+    {image:"https://picsum.photos/300/400",title:"This is a seventh title",description:"This is a seventh description",clickEvent:sliderClick},
+    {image:"https://picsum.photos/300/400",title:"This is a seventh title",description:"This is a seventh description",clickEvent:sliderClick},
+    {image:"https://picsum.photos/300/400",title:"This is a seventh title",description:"This is a seventh description",clickEvent:sliderClick},
+  ]
   return (
     <header>
       <CarSlider></CarSlider>
       <Brands></Brands>
       <Welcome></Welcome>
       <Cards></Cards>
+      <Servives></Servives>
+      <ReactCardSlider slides={slides}/>
     </header>
   );
 };
